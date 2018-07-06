67c3a4d4fa27aa068cbf47431104082d108da329
Samuel Nelson <samuel@spring.co.nz>
2018-07-06 13:48:06 +1200

    Initial commit

---
+++ App.js
@@ -0,0 +1,74 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View, TextInput} from 'react-native';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+  constructor(props) {
+    super(props)
+    this.state = {value: ''}
+  }
+
+  handleTextChange(v) {
+	this.setState({value: v})
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
+        <TextInput
+          style={styles.text}
+          value={this.state.value}
+          multiline
+          onChangeText={v => {
+              this.setState({
+                  value: v
+              })
+          }}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  text: {
+    flex: 1,
+    alignSelf: 'stretch',
+    borderColor: '#555555',
+    borderWidth: 1,
+  }
+});
